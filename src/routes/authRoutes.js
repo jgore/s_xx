b47f79e20bcdb68041ff3bc53b2f9bc5fe59a1ba
codemasternode <marcinwarzybok@outlook.com>
2019-08-15 23:53:57 +0200

    merged

--- src/routes/authRoutes.js
+++
@@ -1,7 +0,0 @@
-import { loginUser, registerUser } from "../controllers/authControllers";
-
-export default router => {
-  router.post("/login", loginUser);
-  router.post("/register", registerUser);
-  return router;
-};
