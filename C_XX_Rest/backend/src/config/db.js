0b76c378ef489fdfc89a84293def3ba40398ddeb
codemasternode <marcinwarzybok@outlook.com>
2019-08-22 23:57:11 +0200

    prepare query.json

--- C_XX_Rest/backend/src/config/db.js
+++ C_XX_Rest/backend/src/config/db.js
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
-import {loadData} from '../services/loadData'
-import Product from '../models/products'
-import Serial from '../models/serial'
+import { loadData } from "../services/loadData";
+import Product from "../models/products";
+import Serial from "../models/serial";
+import uuid from "uuid";
+import fs from "fs";
+import path from "path";
 
 export default async URI => {
   mongoose.connect(URI, { useNewUrlParser: true }, err => {
@@ -10,20 +13,43 @@ export default async URI => {
     }
     console.log(`Connected to MongoDB on port ${URI}`);
   });
-  await Product.deleteMany({})
-  await Serial.deleteMany({})
-  const data = await loadData(["mockData/products.json", "mockData/serial.json"])
-  
-  const products = await Product.insertMany(data[0])
-  const unSavedSerials = data[1]
-  for(let i = 0; i < products.length; i++) {
-    for(let k = 0; k < unSavedSerials.length; k++) {
-      if(unSavedSerials[k].name === products[i].name) {
-        unSavedSerials[k].product = products[i]._id
+
+  await Product.deleteMany({});
+  await Serial.deleteMany({});
+  const data = await loadData([
+    "mockData/products.json",
+    "mockData/serial.json"
+  ]);
+
+  const products = await Product.insertMany(data[0]);
+  const unSavedSerials = data[1];
+  for (let i = 0; i < products.length; i++) {
+    for (let k = 0; k < unSavedSerials.length; k++) {
+      if (unSavedSerials[k].name === products[i].name) {
+        unSavedSerials[k].product = products[i]._id;
       }
-    }  
+    }
   }
 
-  const serials = await Serial.insertMany(unSavedSerials)
+  const generatedSerials = [];
+  for (let i = 0; i < 10000; i++) {
+    generatedSerials.push({
+      serial: uuid(),
+      product: products[0]._id
+    });
+  }
 
+  const serials = await Serial.insertMany([
+    ...unSavedSerials,
+    ...generatedSerials
+  ]);
+
+  const onlyIDS = serials.map((value) => {
+    return value.serial
+  })
+
+  fs.writeFileSync(
+    path.join(__dirname, "./query.json"),
+    JSON.stringify(onlyIDS, null, 3)
+  );
 };
--- C_XX_Rest/backend/test/routesCompany.spec.js
+++ C_XX_Rest/backend/test/routesCompany.spec.js
@@ -2,8 +2,7 @@ import "babel-polyfill";
 import chai, { assert } from "chai";
 import server from "../index";
 import chaiHttp from "chai-http";
-import Company from "../models/company";
-import Country from "../models/country";
+import Serial from '../src/models/serial'
 import { Types } from "mongoose";
 
 const { expect } = chai;
@@ -17,216 +16,27 @@ function validResponse(err, res, status) {
   expect(res.body).to.be.an("object");
 }
 
+let serials = []
+
 describe("get one page of companies", function() {
+  beforeEach(() => {
+    return new Promise((resolve) => {
+      Serial.find({}).limit(600).then((docs) => {
+        serials = docs.map((value) => {
+          return value._id
+        })
+        resolve()
+      })
+    })
+  })
   it("should return page with companies", function(done) {
     chai
       .request(server)
-      .get("/api/company/page/1")
+      .post("/api/get-drugs")
+      .send()
       .end((err, res) => {
         validResponse(err, res, 200);
-        expect(res.body).to.have.all.keys("companies", "numberOfCompanies");
-        expect(res.body.companies).to.be.an("array");
-        expect(res.body.numberOfCompanies).to.be.a("number");
         done();
       });
   });
 });
-
-describe("get company by id", function() {
-  it("should return 200", function(done) {
-    Company.find({}).then(companies => {
-      chai
-        .request(server)
-        .get(`/api/company/${companies[0]._id}`)
-        .end((err, res) => {
-          validResponse(err, res, 200);
-          expect(res.body).to.have.property("_id");
-          expect(res.body._id).to.equal(companies[0]._id.toString());
-          done();
-        });
-    });
-  });
-
-  it("should return 404", function(done) {
-    chai
-      .request(server)
-      .get("/api/company/abc")
-      .end((err, res) => {
-        validResponse(err, res, 404);
-        done();
-      });
-  });
-});
-
-describe("create new company", function() {
-  let company = {
-    nameOfCompany: "Transport Tech Service 10 ",
-    name: "Marcin",
-    surname: "Warzybok",
-    taxNumber: "6793184304",
-    plan: "Enterprise",
-    email: "admin@teachtechservice.com",
-    country: "PL"
-  };
-  it("should return 201 on creating company", function(done) {
-    chai
-      .request(server)
-      .post("/api/company")
-      .send(company)
-      .end((err, res) => {
-        validResponse(err, res, 201);
-        done();
-      });
-  });
-
-  it("should return 202 on foreign country", function(done) {
-    chai
-      .request(server)
-      .post("/api/company")
-      .send({
-        ...company,
-        country: "DE"
-      })
-      .end((err, res) => {
-        validResponse(err, res, 202);
-        expect(res.body).to.have.property("msg");
-        done();
-      });
-  });
-
-  it("should return 400 on missing required property", function(done) {
-    let { email, ...wrongCompany } = company;
-    chai
-      .request(server)
-      .post("/api/company")
-      .send(wrongCompany)
-      .end((err, res) => {
-        validResponse(err, res, 400);
-        done();
-      });
-  });
-
-  it("should return 400 on missing country", function(done) {
-    let { country, ...wrongCompany } = company;
-    chai
-      .request(server)
-      .post("/api/company")
-      .send(wrongCompany)
-      .end((err, res) => {
-        validResponse(err, res, 400);
-        done();
-      });
-  });
-
-  it("should return 400 on wrong country", function(done) {
-    chai
-      .request(server)
-      .post("/api/company")
-      .send({
-        ...company,
-        country: "XD"
-      })
-      .end((err, res) => {
-        validResponse(err, res, 400);
-        done();
-      });
-  });
-});
-
-describe("update company-info on existing company", function() {
-  let update = {
-    margin: 20
-  };
-
-  it("should return 200 on updating company", function(done) {
-    Company.find({}).then(companies => {
-      chai
-        .request(server)
-        .put(`/api/company/company-info/${companies[0]._id}`)
-        .send({ update })
-        .end((err, res) => {
-          validResponse(err, res, 200);
-          done();
-        });
-    });
-  });
-
-  it("should return 200 on correct country while update", function(done) {
-    Company.find({}).then(companies => {
-      chai
-        .request(server)
-        .put(`/api/company/company-info/${companies[0]._id}`)
-        .send({ update: { ...update, country: "PL" } })
-        .end((err, res) => {
-          validResponse(err, res, 200);
-          done();
-        });
-    });
-  });
-
-  it("should return 400 on unallowed property", function(done) {
-    Company.find({}).then(companies => {
-      chai
-        .request(server)
-        .put(`/api/company/company-info/${companies[0]._id}`)
-        .send({ update: { ...update, abc: "unallowe property" } })
-        .end((err, res) => {
-          validResponse(err, res, 400);
-          done();
-        });
-    });
-  });
-
-  it("should return 400 on missing company", function(done) {
-    chai
-      .request(server)
-      .put(`/api/company/company-info/notworking`)
-      .send({ update })
-      .end((err, res) => {
-        validResponse(err, res, 404);
-        done();
-      });
-  });
-
-  it("should return 400 on missing country while update", function(done) {
-    Company.find({}).then(companies => {
-      chai
-        .request(server)
-        .put(`/api/company/company-info/${companies[0]._id}`)
-        .send({ update: { ...update, country: "XD" } })
-        .end((err, res) => {
-          validResponse(err, res, 400);
-          done();
-        });
-    });
-  });
-});
-
-describe("delete company by admin", function() {
-  let company = {
-    nameOfCompany: "Transport Tech Service 10 ",
-    name: "Marcin",
-    surname: "Warzybok",
-    taxNumber: "6793184304",
-    plan: "Enterprise",
-    email: "admin@teachtechservice.com",
-    _id: new Types.ObjectId()
-  };
-
-  it("should return 200 on delete", function(done) {
-    Country.find({ name: "PL" }).then(country => {
-      company.country = country._id;
-      console.log(company);
-      Company.create(company, function(savedCompany) {
-        console.log(savedCompany);
-        done();
-      });
-    });
-  });
-});
-
-describe("delete company by company", function() {});
-
-describe("update tax-info on existing company", function() {});
-
-describe("update company email", function() {});
